c14cdb76d82862e7da141264baba1f38e25eb617
Jenn <39388566+strawberries73@users.noreply.github.com>
2022-07-14 13:05:35 -0400

    Second commit
    Form Logic for react hooks to reset state

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-/* eslint-disable no-template-curly-in-string */
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { useInput } from "./useInput";
 
 // Handling our form values by assigning them to state values
 function App() {
-  const [sound, setSound] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    alert('${sound} sounds like ${color}');
+    alert('${titleProps.value} sounds like ${colorProps.value}');
     // Reset the sound and color
-    setSound("");
-    setColor("#000000");
+    resetTitle();
+    resetColor();
   };
 
    return (
     <form onSubmit={submit}>
       <input
-        value={sound} 
+        {...titleProps} 
          type="text" 
          placeholder="Sound..." 
-         onChange={(e) => setSound(e.target.value)}
       />
-      <input value={color} type="color"
-        onChange={(e) => setColor(e.target.value)}
-      />
+      <input {...colorProps} type="color"/>
       <button>ADD</button>
     </form>
   );
@@ -39,4 +36,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
